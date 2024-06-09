37a86d2cc79cfd77b3ba5ed41fe6eff80a269cd3
João Pedro Pires Barçante <joaopedropires61@gmail.com>
2024-06-09 19:55:21 -0300

    Alterar style

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+:
+
+scripts.js,8\e\8e28a44cf348c4821ee49ae3286882da2a9d231d
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
--- horario.js
+++ horario.js
@@ -1,20 +1,18 @@
 function atualizaHorario() {
-    var xhr = new XMLHttpRequest();
-    var url = "horario.php"
+    var request = new XMLHttpRequest();
+    var url = "horario.php";
 
-    xhr.open("GET", url, true);
+    request.open("GET", url, true);
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 3) {
-            console.log('Carregando o conteúdo');
-        }
-        if (xhr.readyState == 4) {
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            console.log('Conteúdo carregado');
             var div_horario = document.getElementById("horario");
-            div_horario.innerHTML = xhr.responseText;
+            div_horario.innerHTML = request.responseText;
         }
     };
 
-    xhr.send(null);
+    request.send(null);
 }
 
 setInterval(atualizaHorario, 1000);
